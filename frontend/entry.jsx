a7ad21dc4922eebbb715c75e59ff61a0300553f2
Dustin Pearson <dustinparkerpearson@gmail.com>
2018-08-04 17:00:50 -0700

    Initial commit of new rails/react project

---
+++ frontend/entry.jsx
@@ -0,0 +1,8 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>We can render!</h1>, root);
+});
