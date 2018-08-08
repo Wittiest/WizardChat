4ecc7ebb21060fb3be4ee0572d54afb4cf17e53b
Dustin Pearson <dustinparkerpearson@gmail.com>
2018-08-08 00:54:46 -0700

    Fix casing error for heroku

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,7 @@
 // React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './components/root';
+import Root from './components/Root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
