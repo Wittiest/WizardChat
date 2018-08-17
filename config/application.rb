9b04ee7e6a7f02271cdee778ee78577977ef48ec
Dustin Pearson <dustinparkerpearson@gmail.com>
2018-08-17 08:18:04 -0700

    Force server time-zone to be PDT

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module WizardChat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.time_zone = "Pacific Time (US & Canada)"
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
